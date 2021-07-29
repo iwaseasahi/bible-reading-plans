b132aee97be89c94080435468bec45b2595a83c6
岩瀬旭 <eb50113@yahoo.co.jp>
2021-07-29 10:39:26 +0900

    Create TestamentScreen

---
+++ src/screens/Testament.tsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import { StyleSheet, View, FlatList } from 'react-native';
+import { List } from 'react-native-paper';
+
+const Stack = createStackNavigator();
+
+type tastaments = {
+  id: number;
+  text: string;
+}[]
+
+const tastaments = [
+  {
+    id: 1,
+    text: '旧約聖書',
+  },
+  {
+    id: 2,
+    text: '新約聖書',
+  }
+]
+
+export const TestamentScreen = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="聖書"
+        component={listItems}
+      />
+    </Stack.Navigator>
+  );
+}
+
+const listItems = () => {
+  return (
+    <View style={styles.container}>
+      <FlatList
+        style={styles.list}
+        data={tastaments}
+        keyExtractor={item => `${item.id}`}
+        renderItem={({ item }) => (
+          <List.Item
+            style={styles.item}
+            title={item.text}
+          />
+        )}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  list: {
+    flex: 1,
+  },
+  item: {
+    borderBottomWidth: 1,
+    borderBottomColor: '#e4e6e8',
+  }
+});
