8ab436621030a4754c42576edaa7b0d6ceabdc69
Asahi Iwase <asahi.iwase.engineer@gmail.com>
2021-08-28 22:08:51 +0900

    章のマスターデータを firebase から取得する

--- src/screens/Chapter.tsx
+++ src/screens/Chapter.tsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import { StyleSheet, View, FlatList } from 'react-native';
 import { CheckBox } from 'react-native-elements'
 
+import firebaseApp from '../initializers/Firebase';
+
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 type Chapters = {
@@ -16,99 +18,6 @@ type Chapter = {
   number: number;
 }
 
-const chapters: Chapters = [
-  {
-    id: 1,
-    bookId: 1,
-    number: 1,
-  },
-  {
-    id: 2,
-    bookId: 1,
-    number: 2,
-  },
-  {
-    id: 3,
-    bookId: 1,
-    number: 3,
-  },
-  {
-    id: 4,
-    bookId: 2,
-    number: 1,
-  },
-  {
-    id: 5,
-    bookId: 2,
-    number: 2,
-  },
-  {
-    id: 6,
-    bookId: 2,
-    number: 3,
-  },
-  {
-    id: 7,
-    bookId: 3,
-    number: 1,
-  },
-  {
-    id: 8,
-    bookId: 3,
-    number: 2,
-  },
-  {
-    id: 9,
-    bookId: 3,
-    number: 3,
-  },
-  {
-    id: 10,
-    bookId: 4,
-    number: 1,
-  },
-  {
-    id: 11,
-    bookId: 4,
-    number: 2,
-  },
-  {
-    id: 12,
-    bookId: 4,
-    number: 3,
-  },
-  {
-    id: 13,
-    bookId: 5,
-    number: 1,
-  },
-  {
-    id: 14,
-    bookId: 5,
-    number: 2,
-  },
-  {
-    id: 15,
-    bookId: 5,
-    number: 3,
-  },
-  {
-    id: 16,
-    bookId: 6,
-    number: 1,
-  },
-  {
-    id: 17,
-    bookId: 6,
-    number: 2,
-  },
-  {
-    id: 18,
-    bookId: 6,
-    number: 3,
-  },
-]
-
 type Route = {
   params: {
     name: string;
@@ -118,6 +27,7 @@ type Route = {
 
 export const ChapterScreen = ({ route }: { route: Route }) => {
   const { bookId } = route.params;
+  const [chapters, setChapters] = useState<Chapters>([]);
   // 選択した書簡の章を抽出する
   const currentChapters: Chapters = chapters.filter(chapter => chapter.bookId === bookId);
 
@@ -126,9 +36,19 @@ export const ChapterScreen = ({ route }: { route: Route }) => {
     new Array(currentChapters.length).fill(false)
   );
 
+  // 初回 useEffect で、fetchChapters を実行する
+  // chapters の変更をフックして、再度 useEffect が実行される
+  // fetchReadChapters を実行して、通読状態をチェックボックスに反映する
   useEffect(() => {
+    fetchChapters();
     fetchReadChapters(currentChapters);
-  }, [])
+  }, [chapters])
+
+  const fetchChapters = async () => {
+    await firebaseApp.database().ref('chapters').once('value').then((snapshot) => {
+      setChapters(snapshot.val())
+    });
+  };
 
   // ローカルストレージから章の通読状態を state に反映する
   const fetchReadChapters = async (chapters: Chapters) => {
